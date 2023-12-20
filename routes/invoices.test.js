126853bfc6404ad997f610b9d77033e04fd3db73
Derek Biggers <biggd3210@gmail.com>
2023-12-19 22:41:20 -0500

    begin factoring invoices test

--- routes/invoices.test.js
+++ routes/invoices.test.js
@@ -6,14 +6,24 @@ const app = require('../app');
 const db = require('../db');
 
 //setup logic
+let testInvoice;
 let testCompany;
+beforeAll(async () => {
+    let companyResult = await db.query(
+        `INSERT INTO companies (code, name, description)
+        VALUES ('ibm', 'IBM', 'Maker of Intel chips and other comp stuff')
+        RETURNING code, name, description`
+    )
+    testCompany = companyResult.rows[0];
+})
+
 beforeEach(async () => {
     let result = await db.query(
         `INSERT INTO invoices (comp_code, amt, paid)
-        VALUES ('ibm', '143.00', 'false)
+        VALUES ('ibm', '143.00', 'false')
         RETURNING id, comp_code, amt, add_date, paid_date`
     );
-    testCompany = result.rows[0];
+    testInvoice = result.rows[0];
 })
 
 //teardown logic
@@ -23,4 +33,13 @@ afterEach(async () => {
 
 afterAll(async () => {
     await db.end();
+})
+
+describe('GET /invoices', function () {
+    test('get list of all invoices', async () => {
+        const resp = await request(app).get('/invoices');
+        console.log(resp.body)
+        expect(resp.statusCode).toEqual(200);
+        expect(resp.body).toEqual({ invoices: [testInvoice] });
+    })
 })
